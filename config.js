8cbe1f5bd4cfcde60eb14c867e99b652772cf1a8
Hugh <hughchen2046@gmail.com>
2026-04-19 11:12:09 +0800

    update

---
+++ config.js
@@ -0,0 +1,7 @@
+window.ENV = {
+  // Google OAuth Client ID (純前端驗證用，不需 Client Secret)
+  GOOGLE_CLIENT_ID: '54030448384-r66n4d7tpithogqmckb9hi5gbq2kbrlc.apps.googleusercontent.com',
+  
+  // Google Sheet 網址或 Spreadsheet ID
+  GOOGLE_SHEET_ID: '1TAFOBxbBLTHq8fH05BccSFiccoYJx9RNLQTkpV1fCJc'
+};
